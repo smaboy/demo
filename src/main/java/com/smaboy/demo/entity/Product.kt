7f2ee8503008a091ee48f8d85c0ce95c3bfece67
liyongliang3 <liyongliang3@ceair.com>
2020-04-16 18:54:23 +0800

    添加产品基类&添加kotlin插件支持

---
+++ src/main/java/com/smaboy/demo/entity/Product.kt
@@ -0,0 +1,7 @@
+package com.smaboy.demo.entity
+
+//double productPrice;
+//int productType;
+//String productImg;
+//String productDes;
+data class Product(var productName : String , var productPrice : Double , var productType : Int , var productImg : String , var productDes : String)
---
+++ src/main/java/com/smaboy/demo/entity/User.kt
@@ -0,0 +1,3 @@
+package com.smaboy.demo.entity
+
+data class User  @JvmOverloads constructor(var username : String = "", var password : String = "", var phone : String = "")
